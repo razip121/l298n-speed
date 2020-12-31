926d01b7592b8ab0e8f64843982dc8769e296dda
Pascal Watteel <pascal@watteel.be>
2020-12-31 22:43:08 +0400

    fix email

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setuptools.setup(
     name="l298n-speed",
     version="0.0.1",
     author="Pascal Watteel",
-    author_email="pascal@watteelcom",
+    author_email="pascal@watteel.com",
     description="A module to assist in controlling an L298N H-Bridge motor driver including speed control via pwm.",
     long_description=long_description,
     long_description_content_type="text/markdown",
